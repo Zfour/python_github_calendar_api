74e6eea1e2636308c7451087bf81f466f5990b76
Zfour <499984532@qq.com>
2021-01-18 21:14:49 +0800

    111

--- api/index.py
+++ api/index.py
@@ -33,5 +33,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write(json.dumps(name).encode('utf-8'))
+        self.wfile.write(name.encode('utf-8'))
         return
