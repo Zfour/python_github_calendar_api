fdf2b6671fcfaf5f97f9de56162b58c5240c25fc
Zfour <499984532@qq.com>
2021-01-18 22:34:38 +0800

    尝试解决跨域

--- api/index.py
+++ api/index.py
@@ -33,6 +33,7 @@ class handler(BaseHTTPRequestHandler):
         user = path.split('?')[1]
         data = getdata(user)
         self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type','application/json')
         self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
