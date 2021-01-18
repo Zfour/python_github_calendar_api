81ebd2cd8752226ccb187cbe78f88183901d8c19
Zfour <499984532@qq.com>
2021-01-18 21:04:54 +0800

    测试

--- api/index.py
+++ api/index.py
@@ -4,36 +4,35 @@ import re
 from http.server import BaseHTTPRequestHandler
 import json
 
-gitpage = requests.get("https://github.com/Zfour")
-data = gitpage.text
+def getdata(name):
+    gitpage = requests.get("https://github.com/" +name)
+    data = gitpage.text
+    datadatereg = re.compile(r'data-date="(.*?)"/>')
+    datacountreg = re.compile(r'data-count="(.*?)" data-date')
+    datadate = datadatereg.findall(data)
+    datacount = datacountreg.findall(data)
+    datacount =list(map(int, datacount))
+    contributions = sum(datacount)
+    datalist=[]
+    for index,item in enumerate(datadate):
+        itemlist = {"date":item,"count":datacount[index]}
+        datalist.append(itemlist)
+    returndata = {
+        "tatal":contributions,
+        "contributions":[
+            datalist
+      ]
 
-contributionsreg = re.compile('<h2 class="f4 text-normal mb-2">(.*?)contributions')
-datadatereg = re.compile(r'data-date="(.*?)"/>')
-datacountreg = re.compile(r'data-count="(.*?)" data-date')
-
-datadate = datadatereg.findall(data)
-datacount = datacountreg.findall(data)
-datacount =list(map(int, datacount))
-contributions = sum(datacount)
-
-datalist=[]
-for index,item in enumerate(datadate):
-    itemlist = {"date":item,"count":datacount[index]}
-    datalist.append(itemlist)
-
-returndata = {
-    "tatal":contributions,
-    "contributions":[
-        datalist
-  ]
-
-}
+    }
+    return returndata
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        name = self.headers.get('Location').encode()
+
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        self.wfile.write(json.dumps(returndata).encode('utf-8'))
+        self.wfile.write(json.dumps(name).encode('utf-8'))
         return
