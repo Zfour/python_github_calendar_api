42e9365486fe0fdea8de8b9108c37536246202b3
Zfour <499984532@qq.com>
2021-01-20 23:21:46 +0800

    调整

--- api/index.py
+++ api/index.py
@@ -5,39 +5,34 @@ from http.server import BaseHTTPRequestHandler
 import json
 
 def list_split(items, n):
-    return [items[i:i+n] for i in range(0, len(items), n)]
-
-
+    return [items[i:i + n] for i in range(0, len(items), n)]
 def getdata(name):
-    gitpage = requests.get("https://github.com/" +name)
+    gitpage = requests.get("https://github.com/" + name)
     data = gitpage.text
     datadatereg = re.compile(r'data-date="(.*?)"/>')
     datacountreg = re.compile(r'data-count="(.*?)" data-date')
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
-    datacount =list(map(int, datacount))
+    datacount = list(map(int, datacount))
     contributions = sum(datacount)
-    datalist=[]
-    for index,item in enumerate(datadate):
-        itemlist = {"date":item,"count":datacount[index]}
+    datalist = []
+    for index, item in enumerate(datadate):
+        itemlist = {"date": item, "count": datacount[index]}
         datalist.append(itemlist)
     datalistsplit = list_split(datalist, 7)
     returndata = {
-        "total":contributions,
-        "contributions":datalistsplit
+        "total": contributions,
+        "contributions": datalistsplit
     }
-
     return returndata
-
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
         path = self.path
         user = path.split('?')[1]
         data = getdata(user)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-type','application/json')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
         return
