c31dd859e27bcd6b489204f6350b07564471bda8
Zkeq <admin@icodeq.com>
2023-01-06 10:43:08 +0800

    Github 页面改版，更新 `regex` 规则

--- api/index.py
+++ api/index.py
@@ -10,10 +10,10 @@ def getdata(name):
     gitpage = requests.get("https://github.com/" + name)
     data = gitpage.text
     datadatereg = re.compile(r'data-date="(.*?)" data-level')
-    datacountreg = re.compile(r'data-count="(.*?)" data-date')
+    datacountreg = re.compile(r'rx="2" ry="2">(.*?) contribution')
     datadate = datadatereg.findall(data)
     datacount = datacountreg.findall(data)
-    datacount = list(map(int, datacount))
+    datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
     contributions = sum(datacount)
     datalist = []
     for index, item in enumerate(datadate):
